29517de8011189b115b4d6c77edb1f918b4dc206
prasetion <prasetion@gmail.com>
2025-08-11 23:16:12 +0700

    handling validation input form using onblur

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -6,16 +6,68 @@ export default function StateLogin() {
         password: ''
     })
 
+    const [didEdit, setDidEdit] = useState({
+        email: false,
+        password: false
+    })
+
+    const emailIsInvalid = didEdit.email && !enteredValue.email.includes('@');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(enteredValue);
+    }
+
     function handleInputChange(identifier, value) {
         setEnteredValue(prevState => ({
             ...(prevState),
             [identifier]: value
         }))
+        setDidEdit((prevState) => (
+            {
+                ...prevState,
+                [identifier]: false
+            }
+        ))
+    }
+
+    function handleInputBlur(identifier) {
+        setDidEdit((prevState) => (
+            {
+                ...prevState,
+                [identifier]: true
+            }
+        ))
     }
 
     return (
-        <div>
+        <form>
+            <h2>Login</h2>
 
-        </div>
+            <div className="control-row">
+                <div className="control no-margin">
+                    <label htmlFor="email">Email</label>
+                    <input id="email" type="email" name="email"
+                           onBlur={() => handleInputBlur('email')}
+                           value={enteredValue.email}
+                           onChange={event => handleInputChange('email', event.target.value)}/>
+                    <div className="control-error">
+                        {emailIsInvalid && <p>Please enter a valid email</p>}
+                    </div>
+                </div>
+
+                <div className="control no-margin">
+                    <label htmlFor="password">Password</label>
+                    <input id="password" type="password" name="password"
+                           value={enteredValue.password}
+                           onChange={event => handleInputChange('password', event.target.value)}/>
+                </div>
+            </div>
+
+            <p className="form-actions">
+                <button className="button button-flat">Reset</button>
+                <button className="button" onClick={handleSubmit}>Login</button>
+            </p>
+        </form>
     )
 }
