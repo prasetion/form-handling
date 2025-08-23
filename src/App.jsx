eeff464f3dfb1da3cd3d5faf1030f0da3651c255
prasetion <prasetion@gmail.com>
2025-08-23 23:53:35 +0700

    built in validation input by browser

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ function App() {
         <>
             <Header/>
             <main>
-                <StateLogin/>
+                {/*<StateLogin/>*/}
+                <Signup/>
             </main>
         </>
     );
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -15,13 +15,13 @@ export default function Signup() {
 
             <div className="control">
                 <label htmlFor="email">Email</label>
-                <input id="email" type="email" name="email"/>
+                <input id="email" type="email" name="email" required minLength="5"/>
             </div>
 
             <div className="control-row">
                 <div className="control">
                     <label htmlFor="password">Password</label>
-                    <input id="password" type="password" name="password"/>
+                    <input id="password" type="password" name="password" required/>
                 </div>
 
                 <div className="control">
@@ -30,6 +30,7 @@ export default function Signup() {
                         id="confirm-password"
                         type="password"
                         name="confirm-password"
+                        required
                     />
                 </div>
             </div>
@@ -39,12 +40,12 @@ export default function Signup() {
             <div className="control-row">
                 <div className="control">
                     <label htmlFor="first-name">First Name</label>
-                    <input type="text" id="first-name" name="first-name"/>
+                    <input type="text" id="first-name" name="first-name" required/>
                 </div>
 
                 <div className="control">
                     <label htmlFor="last-name">Last Name</label>
-                    <input type="text" id="last-name" name="last-name"/>
+                    <input type="text" id="last-name" name="last-name" required/>
                 </div>
             </div>
 
@@ -89,7 +90,7 @@ export default function Signup() {
 
             <div className="control">
                 <label htmlFor="terms-and-conditions">
-                    <input type="checkbox" id="terms-and-conditions" name="terms"/>I
+                    <input type="checkbox" id="terms-and-conditions" name="terms" required/>I
                     agree to the terms and conditions
                 </label>
             </div>
